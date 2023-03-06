c654284e0fd7261922123d74942422dc1efbde2a
12_web_hjs <12_web_hjs@DESKTOP-2RF42K3>
2023-03-06 14:38:31 +0900

    최초 커밋

---
+++ 09_java_OOP_advanced/src/step9_04/student/StudentDAO.java
@@ -0,0 +1,19 @@
+package step9_04.student;
+//가공된(Controller <->DAO) 데이터를 DB로 보내기 전
+import java.util.Map;
+
+public class StudentDAO {
+	
+	public void insert(StudentVO st) {
+		StudentRepository.getStDB().put(st.getId(), st);
+	}
+	
+	public StudentVO select(String id) {
+		return StudentRepository.getStDB().get(id);
+	}
+	
+	public Map<String , StudentVO> getStudentDB(){
+		return StudentRepository.getStDB();
+	}
+
+}
---
+++ 09_java_OOP_advanced/src/step9_04/student/StudentSelectAll.java
@@ -0,0 +1,30 @@
+package step9_04.student;
+
+//서비스 부분
+import java.util.Map;
+
+public class StudentSelectAll {
+	
+	private StudentDAO studentDAO;
+	
+	public StudentSelectAll(StudentDAO studentDAO) {
+		this.studentDAO = studentDAO;
+	}
+	
+	
+	public Map<String , StudentVO> allSelect(){
+		return studentDAO.getStudentDB();
+	}
+	
+	
+	public void printAll() {
+		
+		Map<String , StudentVO> map = studentDAO.getStudentDB();
+		
+		for (String key : map.keySet()) {
+			map.get(key).printOneInfo();
+		}
+		
+	}
+	
+}
